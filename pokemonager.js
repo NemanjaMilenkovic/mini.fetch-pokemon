dfebfdaf145bdeed2f097512c65c6f158f8cc09d
NemanjaMilenkovic <nemanja.milenkovic@gmail.com>
2020-01-18 21:35:04 +0900

    Finished findUnderWeight()

--- pokemonager.js
+++ pokemonager.js
@@ -1,21 +1,21 @@
 (() => {
   class Pokemonager {
-
-
-
-
     // This should return an array of all the names of n Pokemon from the Pokemon API.
 
     findNames(n) {
-      const pokemonArray = fetch('https://pokeapi.co/api/v2/pokemon/?limit=' + n).then((resolve) => {
-        return resolve.json();
-      }).then((array) => {
-        let pokemonArray = array.results;
-        return pokemonArray.map((pokemon => {
-          return pokemon.name;
-        }));
-      }
-      ).catch((err => console.log("There was an error")));
+      const pokemonArray = fetch(
+        `https://pokeapi.co/api/v2/pokemon/?limit=${n}`
+      )
+        .then((resolve) => {
+          return resolve.json();
+        })
+        .then((array) => {
+          let pokemonArray = array.results;
+          return pokemonArray.map((pokemon) => {
+            return pokemon.name;
+          });
+        })
+        .catch((err) => console.log("There was an error"));
       return pokemonArray;
     }
 
@@ -25,33 +25,26 @@
       // Your code here.
       // ** LIMIT TO THE FIRST 10 POKEMON
       // We don't want to make too many unnecessary calls to the Pokemon API
-      const pokemonArray = fetch('https://pokeapi.co/api/v2/pokemon/?limit=10').then((resolve) => {
-        return resolve.json();
-      }).then((array) => {
-        let pokemonArray = array.results;
-        let results = [];
-        let newPokemonUrlArray = [];
-        pokemonArray.filter((pokemon => {
-          // console.log(pokemon.url);
-          newPokemonUrlArray.push(pokemon.url);
-        }));
-        for (let url of newPokemonUrlArray) {
-          fetch(url).then((resolve) => {
-            return resolve.json();
-          }).then((arr) => {
-            if (arr.weight < weight) results.push(arr[0]);
-            // console.log(arr.weight);
 
-          })
+      const getPokemons = (url) => fetch(url);
+
+      const findPokemonsByWeight = async () => {
+        const pokemonApiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=10";
+        const pokemonObject = await (await getPokemons(pokemonApiUrl)).json();
+        const pokemonsUrls = pokemonObject.results.map((pokemon) => {
+          return pokemon.url;
+        });
+        const pokemonsByWeight = [];
+
+        for (let url of pokemonsUrls) {
+          const pokemonFullObject = await (await getPokemons(url)).json();
+          if (pokemonFullObject.weight < weight)
+            pokemonsByWeight.push(pokemonFullObject);
         }
-        console.log("this is a result -------> ", results);
-
-        return results;
-      }
-      ).catch((err => console.log("There was an error")));
-      return pokemonArray;
+        return pokemonsByWeight;
+      };
+      return findPokemonsByWeight();
     }
   }
-
   window.Pokemonager = Pokemonager;
 })();
